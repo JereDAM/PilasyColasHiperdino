099fae692f9f379ea110894a9e48e66ebcf37a64
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-03 21:43:16 +0200

    Feat(development): Se añadió atributos y metodo para retornar un producto aleatorio

---
+++ es/hiperdino/producto/producto.java
@@ -0,0 +1,18 @@
+package es.hiperdino.producto;
+
+import java.util.Random;
+
+public class producto {
+    
+    private String nombreProducto;
+    private static String []arrayProductos = {"Jamon", "Queso", "Fanta", "Helado", "Fresas", "Arandanos", "Frambuesas", "Plátanos", "Sardinas", "Pan", "Col", "Croissant"};
+
+
+    public static String productoAleatorio() {
+        
+        Random productoRandom = new Random();
+
+        int productoAleatorio = productoRandom.nextInt(arrayProductos.length);
+        return arrayProductos[productoAleatorio];
+    }
+}
