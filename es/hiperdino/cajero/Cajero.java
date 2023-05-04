430eb1d18bb1c1c45f423c3316ded3ef36107c93
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-04 12:27:56 +0100

    Feature(cajero): Se añadio metodos de añadir cliente y quitar cliente

--- es/hiperdino/cajero/Cajero.java
+++ es/hiperdino/cajero/Cajero.java
@@ -5,9 +5,18 @@ import java.util.Queue;
 
 import es.hiperdino.cliente.Cliente;
 
+
 public class Cajero {
 
     Queue<Cliente> colaClientes = new LinkedList<>();
 
+    Cliente nuevoCliente = new Cliente();
 
+    public void anadirCliente(){
+        colaClientes.add(nuevoCliente);
+    }
+
+    public void siguienteCliente(){
+        colaClientes.poll();
+    }
 }
