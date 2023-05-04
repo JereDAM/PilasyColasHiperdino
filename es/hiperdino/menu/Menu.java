e622d44146160454c66ca6cff1dc8fce13d189ff
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-04 13:38:56 +0100

    Feat(Menu): Se añadió un metodo menú

--- es/hiperdino/menu/Menu.java
+++ es/hiperdino/menu/Menu.java
@@ -1,5 +1,30 @@
 package es.hiperdino.menu;
 
+import es.hiperdino.cajero.Cajero;
+
 public class Menu {
+
+    private static int aparicionMenu = -1;
+    private static int numeroOpcion;
     
+    public static void Menuhiperdino(){
+
+        while (aparicionMenu != 0) {
+            System.out.println("Selecciona una de las siguientes opciones");
+            System.out.println("1. Abrir caja");
+            System.out.println("2. Añadir nuevo cliente a la cola");
+            System.out.println("3. Atender un cliente");
+            System.out.println("4. Cerrar caja");
+
+            switch (numeroOpcion) {
+                case 1:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+
+        }
+    }
 }
