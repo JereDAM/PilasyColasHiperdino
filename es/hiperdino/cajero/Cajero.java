1bf10abacc916e7e5366f459335339c3a40e8f70
JereDAM <usr3593@salesianos-lacuesta.net>
2023-05-04 12:56:20 +0100

    Feat(cajerotostring): Se sobreescribió el metodo toString y se añadió el atributo numero de caja

--- es/hiperdino/cajero/Cajero.java
+++ es/hiperdino/cajero/Cajero.java
@@ -8,9 +8,12 @@ import es.hiperdino.cliente.Cliente;
 
 public class Cajero {
 
+    private final int NUMERO_CAJA = 18;
     Queue<Cliente> colaClientes = new LinkedList<>();
 
     Cliente nuevoCliente = new Cliente();
+
+
 
     public void anadirCliente(){
         colaClientes.add(nuevoCliente);
@@ -19,4 +22,19 @@ public class Cajero {
     public void siguienteCliente(){
         colaClientes.poll();
     }
+
+    public String toString(){
+
+        String mensajeCajero = "";
+
+        mensajeCajero += "============================== \n";
+        mensajeCajero += "*Número de caja:" + this.NUMERO_CAJA + "\n";
+        mensajeCajero += "*Total de clientes" + colaClientes.size() + "\n";
+        mensajeCajero += "*Clientes en la cola" + "\n";
+        mensajeCajero += colaClientes;
+        mensajeCajero += "==============================";
+
+        return mensajeCajero;
+    }
+
 }
